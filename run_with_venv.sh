85d1a550da45f88cf7ba352771fc77b50ded5839
mvenus2 <movenus99@gmail.com>
2024-10-20 00:59:26 +0200

    Make run_with_venv.sh executable

--- run_with_venv.sh
+++ run_with_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL=de_DE.utf8
+export LANG=de_DE.utf8
 cd /home/bot/RedditVideoMakerBot
 . venv/bin/activate
 python3 main.py
